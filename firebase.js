81f4a273b6bbad5c2b0475838a11c25929e933c8
Brendan Reape <brendanreape88@gmail.com>
2021-11-30 20:00:02 +0000

    auth and db attached

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUo9pQ_kkxPRH7w_H6CW1pBIR2BeeCQ28",
+  authDomain: "tinder-clone-e3120.firebaseapp.com",
+  projectId: "tinder-clone-e3120",
+  storageBucket: "tinder-clone-e3120.appspot.com",
+  messagingSenderId: "838678235152",
+  appId: "1:838678235152:web:77ae3ce7ce13b555976295",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+export { auth, db };
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,10 +1,46 @@
 import React, { createContext, useContext } from "react";
+import * as Google from "expo-google-app-auth";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signOut,
+} from "@firebase/auth";
+import { auth } from "../firebase";
 
 const AuthContext = createContext({});
 
+const config = {
+  androidClientId:
+    "838678235152-smuih0ei5n7iba28s9l9h3f5p01name6.apps.googleusercontent.com",
+  iosClientId:
+    "838678235152-tc36mv0vjmfu5ger3ju89ks77h2g32o0.apps.googleusercontent.com",
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+};
+
 export const AuthProvider = ({ children }) => {
+  const signInWithGoogle = async () => {
+    await Google.logInAsync(config)
+      .then(async (logInResult) => {
+        if (logInResult.type === "success") {
+          const { idToken, accessToken } = logInResult;
+          const credential = GoogleAuthProvider.credential(
+            idToken,
+            accessToken
+          );
+          console.log("auth", auth);
+          console.log("credential", credential);
+          await signInWithCredential(auth, credential);
+        }
+
+        return Promise.reject();
+      })
+      .catch((error) => setError(error));
+  };
+
   return (
-    <AuthContext.Provider value={{ user: "Brendan" }}>
+    <AuthContext.Provider value={{ user: null, signInWithGoogle }}>
       {children}
     </AuthContext.Provider>
   );
