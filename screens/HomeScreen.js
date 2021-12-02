d572761aa8c16684a31a844e3f839b3f1c7f569e
Brendan Reape <brendanreape88@gmail.com>
2021-12-02 19:04:20 +0000

    basic passing and matching completed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/core";
-import React, { useRef } from "react";
+import React, { useRef, useState, useLayoutEffect, useEffect } from "react";
 import {
   View,
   Text,
@@ -12,40 +12,88 @@ import useAuth from "../hooks/useAuth";
 import tw from "tailwind-rn";
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 import Swiper from "react-native-deck-swiper";
+import {
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  setDoc,
+  query,
+  where,
+} from "@firebase/firestore";
+import { db } from "../firebase";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { user, logout } = useAuth();
+  const [profiles, setProfiles] = useState([]);
   const swiperRef = useRef(null);
 
-  console.log("user", user);
+  // my pic https://i.ibb.co/CsbSFVN/psych.jpg
+  console.log("profiles", profiles);
 
-  const DUMMY_DATA = [
-    {
-      firstName: "Adam",
-      lastName: "F",
-      job: "Podcaster",
-      age: 27,
-      photoURL: "https://i.ibb.co/CsbSFVN/psych.jpg",
-      id: 1,
-    },
-    {
-      firstName: "Brian",
-      lastName: "J",
-      job: "Taco Bell Manager",
-      age: 52,
-      photoURL: "https://i.ibb.co/CsbSFVN/psych.jpg",
-      id: 2,
-    },
-    {
-      firstName: "Alexa",
-      lastName: "G",
-      job: "Musician",
-      age: 52,
-      photoURL: "https://i.ibb.co/CsbSFVN/psych.jpg",
-      id: 3,
-    },
-  ];
+  useLayoutEffect(() => {
+    onSnapshot(doc(db, "users", user.uid), (snapshot) => {
+      if (!snapshot.exists()) {
+        navigation.navigate("Modal");
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    let unsub;
+
+    const fetchCards = async () => {
+      const passes = getDocs(collection(db, "users", user.uid, "passes")).then(
+        (snapshot) => snapshot.docs.map((doc) => doc.id)
+      );
+
+      const matches = getDocs(
+        collection(db, "users", user.uid, "matches")
+      ).then((snapshot) => snapshot.docs.map((doc) => doc.id));
+
+      const passedUserIds = passes.length > 0 ? passes : ["test"];
+      const matchedUserIds = matches.length > 0 ? matches : ["test"];
+
+      unsub = onSnapshot(
+        query(
+          collection(db, "users"),
+          where("id", "not-in", [...passedUserIds, ...matchedUserIds])
+        ),
+        (snapshot) => {
+          setProfiles(
+            snapshot.docs
+              .filter((doc) => doc.id !== user.uid)
+              .map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+              }))
+          );
+        }
+      );
+    };
+
+    fetchCards();
+    return unsub;
+  }, []);
+
+  const swipeLeft = async (cardIndex) => {
+    if (!profiles[cardIndex]) return;
+
+    const userSwiped = profiles[cardIndex];
+    console.log(`You swiped PASS on ${userSwiped.displayName}`);
+
+    setDoc(doc(db, "users", user.uid, "passes", userSwiped.id), userSwiped);
+  };
+
+  const swipeRight = async (cardIndex) => {
+    if (!profiles[cardIndex]) return;
+
+    const userSwiped = profiles[cardIndex];
+    console.log(`You swiped MATCH on ${userSwiped.displayName}`);
+
+    setDoc(doc(db, "users", user.uid, "matches", userSwiped.id), userSwiped);
+  };
 
   return (
     <SafeAreaView style={tw("flex-1")}>
@@ -76,16 +124,18 @@ const HomeScreen = () => {
         <Swiper
           ref={swiperRef}
           containerStyle={{ backgroundColor: "transparent" }}
-          cards={DUMMY_DATA}
+          cards={profiles}
           stackSize={5}
           cardIndex={0}
           animateCardOpacity
           verticalSwipe={false}
-          onSwipedLeft={() => {
+          onSwipedLeft={(cardIndex) => {
             console.log("Swiped NOPE");
+            swipeLeft(cardIndex);
           }}
-          onSwipedRight={() => {
+          onSwipedRight={(cardIndex) => {
             console.log("Swiped MATCH");
+            swipeRight(cardIndex);
           }}
           backgroundColor={"#4FD0E9"}
           overlayLabels={{
@@ -107,36 +157,56 @@ const HomeScreen = () => {
               },
             },
           }}
-          renderCard={(card) => (
-            <View
-              key={card.id}
-              style={tw("relative bg-white h-3/4 rounded-xl")}
-            >
-              <Image
-                style={tw("absolute top-0 h-full w-full rounded-xl")}
-                source={{ uri: card.photoURL }}
-              />
+          renderCard={(card) =>
+            card ? (
+              <View
+                key={card.id}
+                style={tw("relative bg-white h-3/4 rounded-xl")}
+              >
+                <Image
+                  style={tw("absolute top-0 h-full w-full rounded-xl")}
+                  source={{ uri: card.photoURL }}
+                />
 
+                <View
+                  style={[
+                    tw(
+                      "absolute bottom-0 bg-white w-full flex-row justify-between items-center h-20 px-6 py-2 rounded-b-xl"
+                    ),
+                    styles.cardShadow,
+                  ]}
+                >
+                  <View>
+                    <Text style={tw("text-xl font-bold")}>
+                      {card.displayName}
+                    </Text>
+                    <Text>{card.job}</Text>
+                  </View>
+                  <View>
+                    <Text style={tw("text-2xl font-bold")}>{card.age}</Text>
+                  </View>
+                </View>
+              </View>
+            ) : (
               <View
                 style={[
                   tw(
-                    "absolute bottom-0 bg-white w-full flex-row justify-between items-center h-20 px-6 py-2 rounded-b-xl"
+                    "relative bg-white h-3/4 rounded-xl justify-center items-center"
                   ),
                   styles.cardShadow,
                 ]}
               >
-                <View>
-                  <Text style={tw("text-xl font-bold")}>
-                    {card.firstName} {card.lastName}
-                  </Text>
-                  <Text>{card.job}</Text>
-                </View>
-                <View>
-                  <Text style={tw("text-2xl font-bold")}>{card.age}</Text>
-                </View>
+                <Text style={tw("font-bold pb-5")}>No more profiles</Text>
+
+                <Image
+                  style={tw("h-20 w-full")}
+                  height={100}
+                  width={100}
+                  source={{ uri: "https://links.papareact.com/6gb" }}
+                />
               </View>
-            </View>
-          )}
+            )
+          }
         />
       </View>
 
