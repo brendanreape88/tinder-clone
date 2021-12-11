33839aa9ede340c7b306f038ee0bbc5b3c4ad994
Brendan Reape <brendanreape88@gmail.com>
2021-12-11 16:14:41 +0000

    added avoid keyboard and dismiss to login / signup screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,3 @@
-import { useNavigation } from "@react-navigation/core";
 import React, { useState } from "react";
 import {
   View,
@@ -7,52 +6,57 @@ import {
   KeyboardAvoidingView,
   Platform,
   TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 import useAuth from "../hooks/useAuth";
 import tw from "tailwind-rn";
 
 const LoginScreen = () => {
-  const { signInWithGoogle, loading, createAccount, signIn } = useAuth();
-  const navigation = useNavigation();
+  const { createAccount, signIn } = useAuth();
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.os === "ios" ? "padding" : "height"}
-      style={tw("flex-1 justify-center items-center")}
-      keyboardVerticalOffset={10}
-    >
-      <View style={tw("w-4/5 justify-center items-center")}>
-        <TextInput
-          style={tw("bg-white w-full h-8 mb-5")}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          style={tw("bg-white w-full h-8 mb-5")}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-      </View>
-      <View style={tw("flex-row w-4/5")}>
-        <TouchableOpacity
-          style={tw("w-1/2 justify-center items-center")}
-          onPress={() => signIn(email, password)}
-        >
-          <Text>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={tw("w-1/2 justify-center items-center")}
-          onPress={() => createAccount(email, password)}
-        >
-          <Text>Register</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={tw("flex-1 justify-center items-center")}
+        keyboardVerticalOffset={10}
+      >
+        <>
+          <View style={tw("w-4/5 justify-center items-center")}>
+            <TextInput
+              style={tw("bg-white w-full h-8 mb-5")}
+              placeholder="Email"
+              value={email}
+              onChangeText={setEmail}
+            />
+            <TextInput
+              style={tw("bg-white w-full h-8 mb-5")}
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+          </View>
+          <View style={tw("flex-row w-4/5")}>
+            <TouchableOpacity
+              style={tw("w-1/2 justify-center items-center")}
+              onPress={() => signIn(email, password)}
+            >
+              <Text>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={tw("w-1/2 justify-center items-center")}
+              onPress={() => createAccount(email, password)}
+            >
+              <Text>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   SafeAreaView,
-  Text,
   TextInput,
   Button,
   KeyboardAvoidingView,
